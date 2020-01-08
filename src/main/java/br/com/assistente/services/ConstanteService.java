22340d78f741cd53c620293e33be37d3513da2f9
gportes <jpconnecta@bol.com.br>
2020-01-07 22:40:35 -0300

    Constantes

--- src/main/java/br/com/assistente/services/ConstanteService.java
+++ src/main/java/br/com/assistente/services/ConstanteService.java
@@ -2,17 +2,27 @@ package br.com.assistente.services;
 
 import br.com.assistente.models.Constante;
 import br.com.assistente.models.ResultMapeamento;
+import br.com.assistente.models.SetupUsuario;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
-import java.util.Collections;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.UncheckedIOException;
 import java.util.Objects;
 import java.util.Set;
 
 import static br.com.assistente.infra.util.UtilCollections.createSet;
+import static java.util.Collections.singleton;
 import static java.util.Objects.requireNonNull;
 import static org.apache.commons.lang3.StringUtils.isEmpty;
 import static org.apache.commons.lang3.StringUtils.replace;
 import static org.apache.commons.lang3.StringUtils.trim;
 import static org.apache.commons.lang3.StringUtils.upperCase;
+import static org.apache.velocity.runtime.RuntimeConstants.RESOURCE_LOADERS;
 
 public class ConstanteService {
 
@@ -78,6 +88,31 @@ public class ConstanteService {
         final Set<Constante> constantes
     ) {
 
-        return Collections.emptySet();
+        final String nomeAutor = SetupUsuario.find().map(SetupUsuario::getAutor).orElse("????");
+
+        final VelocityContext context = new VelocityContext();
+        context.put( "nomeAutor", nomeAutor );
+        context.put( "nomeEnum", nomeEnum );
+//        context.put( "campos", orderByPosicao( campos ) );
+//        context.put( "importsNecessarios", buscarImports( campos ) );
+        context.put( "StringUtils", StringUtils.class );
+
+        final VelocityEngine engine = new VelocityEngine();
+        engine.setProperty( RESOURCE_LOADERS, "classpath" );
+        engine.setProperty( "resource.loader.classpath.class", ClasspathResourceLoader.class.getName());
+        engine.init();
+
+        try ( final StringWriter writer = new StringWriter() ) {
+            final Template template = engine.getTemplate( "/templates/constante.vm" );
+            template.merge( context, writer );
+            return singleton(
+                new ResultMapeamento.Builder()
+                    .comNomeEntidade( "" )
+                    .comConteudoEntidade( writer.toString() )
+                    .build()
+            );
+        } catch ( IOException e) {
+            throw new UncheckedIOException( e );
+        }
     }
 }
