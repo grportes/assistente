8cf0a357eabbbabbe41e1b45fb10f7ff82325b3f
gportes <jpconnecta@bol.com.br>
2020-02-25 20:52:54 -0300

    refactory

--- src/main/java/br/com/assistente/infra/util/UtilJar.java
+++ src/main/java/br/com/assistente/infra/util/UtilJar.java
@@ -1,8 +1,5 @@
 package br.com.assistente.infra.util;
 
-import br.com.assistente.Main;
-import org.apache.log4j.Logger;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.JarURLConnection;
@@ -12,7 +9,8 @@ import java.util.function.Consumer;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
-import static org.apache.commons.lang3.StringUtils.startsWith;
+import static java.util.Objects.requireNonNull;
+import static org.apache.commons.lang3.StringUtils.equalsIgnoreCase;
 import static org.apache.commons.lang3.StringUtils.startsWithIgnoreCase;
 
 public class UtilJar {
@@ -24,27 +22,42 @@ public class UtilJar {
         final Consumer<InputStream> exec
     ) {
 
-        if ( !startsWith( url.getFile(), "http:" ) ) return;
+        if ( equalsIgnoreCase( url.getProtocol() , "jar" ) ) {
 
-        try {
-            final JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
-            final JarFile jarFile = jarURLConnection.getJarFile();
-            final Enumeration<JarEntry> entries = jarFile.entries();
-            while ( entries.hasMoreElements() ) {
-                final JarEntry jarEntry = entries.nextElement();
-                final String nomeArquivo = jarEntry.getName();
-                if ( startsWithIgnoreCase( nomeArquivo, resourceFile ) ) {
-                    final JarEntry fileEntry = jarFile.getJarEntry( nomeArquivo );
-                    try ( final InputStream inputStream = jarFile.getInputStream( fileEntry ) ) {
-                        exec.accept( inputStream );
+            try {
+                final JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
+                final JarFile jarFile = jarURLConnection.getJarFile();
+                final Enumeration<JarEntry> entries = jarFile.entries();
+                while ( entries.hasMoreElements() ) {
+                    final JarEntry jarEntry = entries.nextElement();
+                    final String nomeArquivo = jarEntry.getName();
+                    if ( startsWithIgnoreCase( nomeArquivo, resourceFile ) ) {
+                        final JarEntry fileEntry = jarFile.getJarEntry( nomeArquivo );
+                        try ( final InputStream inputStream = jarFile.getInputStream( fileEntry ) ) {
+                            exec.accept( inputStream );
+                        }
                     }
                 }
+            } catch ( final IOException e ) {
+                throw new RuntimeException( e );
             }
-        } catch ( final IOException e ) {
-            throw new RuntimeException( e );
+
+        } else if ( equalsIgnoreCase( url.getProtocol(), "file" ) ) {
+
+            final ClassLoader classLoader = UtilJar.class.getClassLoader();
+            final URL drivers =  requireNonNull( classLoader.getResource( resourceFile ),"Resource não localizado!" );
+
+            try ( final InputStream in = drivers.openStream() )  {
+                exec.accept( in );
+            } catch ( final IOException e ) {
+                throw new RuntimeException( e );
+            }
+
+        } else {
+
+            throw new IllegalArgumentException( "Protocolo desconhecido: " + url.getProtocol() );
         }
 
     }
-
 
 }
--- src/main/java/br/com/assistente/models/ColunaId.java
+++ src/main/java/br/com/assistente/models/ColunaId.java
@@ -2,25 +2,16 @@ package br.com.assistente.models;
 
 import org.yaml.snakeyaml.Yaml;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.JarURLConnection;
-import java.net.URL;
-import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
-import static java.util.Objects.requireNonNull;
+import static br.com.assistente.infra.util.UtilJar.getInputStream;
 import static java.util.stream.Collectors.toMap;
 import static org.apache.commons.collections4.MapUtils.isNotEmpty;
-import static org.apache.commons.lang3.StringUtils.endsWithIgnoreCase;
 import static org.apache.commons.lang3.StringUtils.equalsIgnoreCase;
 import static org.apache.commons.lang3.StringUtils.lowerCase;
-import static org.apache.commons.lang3.StringUtils.startsWith;
 
 public final class ColunaId {
 
@@ -53,51 +44,9 @@ public final class ColunaId {
     private static Map<String, String> loadCache() {
 
         if ( isNotEmpty( cache ) ) return cache;
-        final URL resource = ColunaId.class.getResource( "" );
-        cache = startsWith( resource.getFile(), "http:" )
-            ? loadCacheFromJar( resource )
-            : loadCacheFromFile( );
+
+        getInputStream("templates/colunas_id.yml", inputStream -> cache = readYaml( inputStream ) );
         return cache;
-    }
-
-
-    private static Map<String, String> loadCacheFromJar( final URL url ) {
-
-        try {
-            final JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
-            final JarFile jarFile = jarURLConnection.getJarFile();
-            final Enumeration<JarEntry> entries = jarFile.entries();
-            while ( entries.hasMoreElements() ) {
-                final JarEntry jarEntry = entries.nextElement();
-                final String nomeArquivo = jarEntry.getName();
-                final boolean arquivoYml = startsWith( nomeArquivo, "templates/" )
-                        && endsWithIgnoreCase( nomeArquivo, "colunas_id.yml" );
-                if ( arquivoYml ) {
-                    final JarEntry fileEntry = jarFile.getJarEntry( nomeArquivo );
-                    try ( final BufferedInputStream bis = new BufferedInputStream( jarFile.getInputStream( fileEntry ) ) ) {
-                        return readYaml( bis );
-                    }
-                }
-            }
-            throw new RuntimeException( "Não localizou arquivo colunas_id.yml!" );
-        } catch ( final IOException e ) {
-            throw new RuntimeException( e );
-        }
-    }
-
-    private static Map<String, String> loadCacheFromFile( ) {
-
-        final ClassLoader classLoader = DriverCnx.class.getClassLoader();
-        final URL drivers =  requireNonNull(
-            classLoader.getResource( "templates/colunas_id.yml" ),
-            "Pasta drivers não localizada!"
-        );
-
-        try ( final InputStream in = drivers.openStream() )  {
-            return readYaml( in );
-        } catch ( final IOException e ) {
-            throw new RuntimeException( e );
-        }
     }
 
     private static Map<String, String> readYaml( final InputStream in ) {
