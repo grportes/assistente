48b1067890f44d272953ebb56a03335214b4b799
guilherme <gportes@arcom.com.br>
2020-02-26 09:41:01 -0300

    Windows - Assinar Jars

--- keystore/assinar-jar.bat
+++ keystore/assinar-jar.bat
@@ -1,22 +1,10 @@
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/assistente-1.0.1.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/commons-collections4-4.4.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/commons-dbutils-1.7.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/commons-lang3-3.9.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/commons-text-1.8.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/javafx-base-13-win.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/javafx-base-13.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/javafx-controls-13-win.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/javafx-controls-13.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/javafx-fxml-13-win.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/javafx-fxml-13.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/javafx-graphics-13-win.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/javafx-graphics-13.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/org.everit.osgi.bundles.net.sourceforge.jtds-1.3.1.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/slf4j-api-1.7.28.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/slf4j-simple-1.7.28.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/snakeyaml-1.25.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/sqlite-jdbc-3.28.0.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/vavr-0.10.2.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/vavr-match-0.10.2.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/velocity-engine-core-2.1.jar assistente
-jarsigner -keystore "../keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$  ../target/lib/log4j-1.2.17.jar assistente
+@echo off
+
+echo "-------------------------------------------------------------------------------"
+echo "------------------- executando assinatura das Libs ----------------------------"
+echo "-------------------------------------------------------------------------------"
+
+for /r %%a in (target\lib\*jar) do (
+    echo "%%a"
+    jarsigner -keystore "keystore/assistente.jks" -storepass Arcom10$ -keypass Arcom10$ %%a assistente
+)
