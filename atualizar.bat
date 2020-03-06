e8b3b574a48baa169e744f424abb5de5c5e05e7c
guilherme <gportes@arcom.com.br>
2020-03-06 12:46:51 -0300

    Bugfix

---
+++ atualizar.bat
@@ -0,0 +1,7 @@
+@echo off
+
+del u:\siga\assistente\*.jar
+del u:\siga\assistente\*.jnlp
+copy /y d:\desenvolvimento\maven\assistente\doc\express\assistente\public\jws\assistente\assistente-win.jnlp u:\siga\assistente\assistente.jnlp
+copy /y d:\desenvolvimento\maven\assistente\target\assistente*.jar u:\siga\assistente
+xcopy /e/y d:\desenvolvimento\maven\assistente\target\lib u:\siga\assistente\lib
