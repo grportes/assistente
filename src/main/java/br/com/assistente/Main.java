afa45d2f428b58f925dec9314fd448ff46b7c55d
grportes <jpconnecta@bol.com.br>
2021-02-08 19:36:05 -0300

    Geração fat-jar

--- src/main/java/br/com/assistente/Main.java
+++ src/main/java/br/com/assistente/Main.java
@@ -1,72 +1,8 @@
 package br.com.assistente;
 
-import br.com.assistente.services.VersaoService;
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.io.IOException;
-
-import static br.com.assistente.infra.db.ConnectionFactory.closeConnection;
-import static br.com.assistente.infra.javafx.Dialog.msgErro;
-import static br.com.assistente.infra.util.UtilArquivo.getResource;
-import static br.com.assistente.infra.util.UtilJar.getInputStream;
-import static java.lang.String.format;
-import static java.lang.Thread.currentThread;
-import static java.lang.Thread.setDefaultUncaughtExceptionHandler;
-import static javafx.application.Platform.runLater;
-import static org.apache.commons.lang3.exception.ExceptionUtils.getRootCause;
-
-public class Main extends Application {
-
-    private static final Logger logger = LogManager.getLogger(Main.class);
-
-    @Override
-    public void start( final Stage stage ) throws IOException {
-        setDefaultUncaughtExceptionHandler( (t, e) -> runLater(() -> showErrorDialog(t, e)) );
-        currentThread().setUncaughtExceptionHandler( this::showErrorDialog );
-
-        final FXMLLoader loader = new FXMLLoader();
-        loader.setLocation( getResource("/fxml/AssistenteView.fxml") );
-        stage.setTitle( format( "Assistente - %s", VersaoService.VERSAO) );
-        stage.setScene( new Scene(loader.load()) );
-        stage.setOnCloseRequest( e -> Platform.exit() );
-        setIconeApp( stage );
-        stage.show();
-    }
-
-    @Override
-    public void stop() {
-        closeConnection();
-    }
-
-    private void setIconeApp( final Stage stage ) {
-        try {
-            getInputStream(
-                "icons/ideia.png",
-                inputStream -> stage.getIcons().add( new Image( inputStream ) )
-            );
-        } catch ( Throwable e ) {
-            logger.error( e );
-        }
-    }
-
-    private void showErrorDialog(
-        final Thread t,
-        final Throwable e
-    ) {
-        final Throwable rootCause = getRootCause(e);
-        if ( !( rootCause instanceof IllegalArgumentException ) ) logger.error( "Erro critico:", e );
-        final String message = rootCause.getMessage();
-        msgErro( message );
-    }
+public class Main  {
 
     public static void main(String[] args) {
-        launch( args );
+        App.main( args );
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,19 +1,17 @@
 module assistente {
+    requires java.sql;
+    requires java.net.http;
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.net.http;
-//    requires java.logging;
-//    requires org.slf4j;
     requires org.apache.logging.log4j;
     requires org.apache.commons.lang3;
+    requires org.apache.commons.collections4;
+    requires org.yaml.snakeyaml;
+    requires io.vavr;
     requires velocity.engine.core;
     requires commons.dbutils;
-    requires org.apache.commons.collections4;
-    requires io.vavr;
-    requires org.yaml.snakeyaml;
-    requires java.sql;
 
     opens br.com.assistente.controllers to javafx.fxml;
     exports br.com.assistente;
-    exports br.com.assistente.models;
+    exports br.com.assistente.models to org.yaml.snakeyaml;
 }
