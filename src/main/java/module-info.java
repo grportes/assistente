2c025511a1a6ca29a985f66bc9c2b2645c33ea2c
guilherme <gportes@arcom.com.br>
2025-02-21 11:06:15 -0300

    Nova versão 1.0.2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,11 +5,11 @@ module assistente {
     requires javafx.fxml;
     requires org.apache.commons.lang3;
     requires org.apache.commons.collections4;
+    requires org.apache.commons.dbutils;
     requires org.slf4j.simple;
     requires org.yaml.snakeyaml;
     requires io.vavr;
     requires velocity.engine.core;
-    requires commons.dbutils;
 
     opens br.com.assistente.controllers to javafx.fxml;
     opens br.com.assistente.models to org.yaml.snakeyaml, velocity.engine.core;
